144a9ccf8ebcb520e80e13b5f8d01d6577b16b96
Hamza <hamzatarar8@gmail.com>
2022-03-21 17:32:19 +0200

    Added Linters. Created main.rb and app.rb for UI. Created methods for the item class.

--- item.rb
+++ item.rb
@@ -1,27 +1,42 @@
+require 'date'
+# frozen_string_literal: true
+
 class Item
-    attr_accessor :publish_date, :archived
-    attr_reader :id
-    attr_writer :author, :source, :label, :genre
-    def initialize(publish_date, archived)
-        @id = Random.rand(1..1000)
-        @publish_date = publish_date
-        @archived = archived
-    end
+  attr_accessor :publish_date, :archived
+  attr_reader :id
+  attr_writer :author, :source, :label, :genre
 
-    # setter methods
-    def genre(genre) 
-        @genre = genre
-    end
+  def initialize(publish_date, archived)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archived = archived
+  end
 
-    def author(author) 
-        @author = author
-    end
+  # setter methods
+  def genre(genre)
+    @genre = genre
+  end
 
-    def source(source) 
-        @source = source
-    end
+  def author(author)
+    @author = author
+  end
 
-    def label(label) 
-        @label = label
-    end
+  def source(source)
+    @source = source
+  end
+
+  def label(label)
+    @label = label
+  end
+
+  def can_be_archived?
+    current = Date.today
+
+    difference_in_days = (@publish_date - current).to_i
+    (difference_in_days / 365.25) > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived? == true
+  end
 end
