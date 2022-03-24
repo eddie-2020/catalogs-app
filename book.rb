0be9d2ac4218ac073b7ed6d7136016beb4ee5ab3
Hamza <hamzatarar8@gmail.com>
2022-03-24 13:29:48 +0200

    Added save-data and to-hash to store input.

--- book.rb
+++ book.rb
@@ -1,7 +1,7 @@
 require_relative './item'
 
 class Book < Item
-  attr_reader :publisher, :cover_state
+  attr_accessor :publisher, :cover_state
 
   def initialize(publisher, cover_state, publish_date)
     super(publish_date)
