c5320015c44e8f3f1e554a8940ddb9ac13012141
Fikerte-T <tofikertesfaye@gmail.com>
2022-03-24 15:49:42 +0300

    Add unit test for music album class

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../musicalbum'
+describe MusicAlbum do
+    describe 'Initialize music album' do
+        before(:each) do
+            @musicalbum = MusicAlbum.new('2022-03/24', true)
+        end
+        it 'creates a new music album' do
+            expect(@musicalbum).to be_an_instance_of MusicAlbum
+        end
+        it 'Music album is on spotify' do
+            expect(@musicalbum.on_spotify).to be true
+        end
+        it 'Music album has method can_be_archived?' do
+            expect(@musicalbum).to respond_to(:can_be_archived?)
+        end
+    end
+end
